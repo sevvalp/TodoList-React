99e388a33dc78a6c520d9ce6b88e785ca9eca1aa
Engin Al <enginal@gmail.com>
2020-05-03 19:29:15 +0300

    TodoList and ListItem are just presentational now

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import ListItem from './ListItem';
+import React from "react";
+import ListItem from "./ListItem";
 
-class TodoList extends React.Component {
-    state={ items : this.props.items}
-
-    removeTodo = (name) => {
-        this.setState({
-            todos: this.state.todos.filter(x => x !== name)
-        });
-    }
-
-    render() {
-        const newList=this.state.items;
-        const renderedList = newList.map((item) => {
-            return <ListItem item={item} removeTodo={this.removeTodo}/>;
-        });
-        return <div>{renderedList}</div>;
-    } 
-}
+const TodoList = ({ items, removeTodo, toggleActive }) => {
+  const renderedList = items.map((item) => {
+    return (
+      <ListItem
+        item={item}
+        key={item.name}
+        toggleActive={toggleActive}
+        removeTodo={removeTodo}
+      />
+    );
+  });
+  return <div>{renderedList}</div>;
+};
 
 export default TodoList;
